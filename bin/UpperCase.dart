112001d557de5aebc84f886583e8b9252176d11e
flutter-lord <richardolojede2@gmail.com>
2024-09-11 20:44:23 +0100

    commit

--- bin/UpperCase.dart
+++ bin/UpperCase.dart
@@ -1,9 +1,10 @@
 import 'dart:math';
 
 void main() {
-  int alphabet = 65 + Random().nextInt(27);
+  int alphabet = 65 + Random().nextInt(26);
 
   print(alphabet);
-  
+
+  // 65 = A;  90 = Z.
   print(String.fromCharCode(alphabet));
 }
