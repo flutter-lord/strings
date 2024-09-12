89bc0710ae5fddacf5ada561998eba6e85354c7b
flutter-lord <richardolojede2@gmail.com>
2024-09-13 00:09:33 +0100

    commit

--- bin/Plate number.dart
+++ bin/Plate number.dart
@@ -1,16 +1,16 @@
 import 'dart:math';
 void main() {
  
-  String Random1 = String.fromCharCode(65 + Random().nextInt(26));
-  String Random2 = String.fromCharCode(65 + Random().nextInt(26));
-  String Random3 = String.fromCharCode(65 + Random().nextInt(26));
+  String random1 = String.fromCharCode(65 + Random().nextInt(26));
+  String random2 = String.fromCharCode(65 + Random().nextInt(26));
+  String random3 = String.fromCharCode(65 + Random().nextInt(26));
 
-  int rand4 = 0 + Random().nextInt(10);
-  int rand5 = 0 + Random().nextInt(10);
-  int rand6 = 0 + Random().nextInt(10);
-  int rand7 = 0 + Random().nextInt(10);
+  int random4 = 0 + Random().nextInt(10);
+  int random5 = 0 + Random().nextInt(10);
+  int random6 = 0 + Random().nextInt(10);
+  int random7 = 0 + Random().nextInt(10);
 
-  String plateNumber = Random1 + Random2 + Random3 + ' ' + '$rand4' + '$rand5' + ' ' + '$rand6' +'$rand7';
+  String plateNumber = random1 + random2 + random3 + ' ' + '$random4' + '$random5' + ' ' + '$random6' + '$random7';
 
   print('The Plate Number of your car is $plateNumber');
 
